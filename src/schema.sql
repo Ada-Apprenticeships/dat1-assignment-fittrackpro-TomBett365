7bc663727d7294062a04597b685fece6e90daa20
TomBett365 <tom.bettridge@ada.ac.uk>
2025-01-30 13:17:32 +0000

    Data imported into database

--- src/schema.sql
+++ src/schema.sql
@@ -99,69 +99,84 @@ CREATE TABLE memberships (
 -- After creating the tables, you can import the sample data using:
 -- `.read data/sample_data.sql` in a sql file or `npm run import` in the terminal
 
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS staff;
+DROP TABLE IF EXISTS equipment;
+DROP TABLE IF EXISTS classes;
+DROP TABLE IF EXISTS class_schedule;
+DROP TABLE IF EXISTS memberships;
+DROP TABLE IF EXISTS attendance;
+DROP TABLE IF EXISTS class_attendance;
+DROP TABLE IF EXISTS payments;
+DROP TABLE IF EXISTS personal_training_sessions;
+DROP TABLE IF EXISTS member_health_metrics;
+DROP TABLE IF EXISTS equipment_maintenance_log;
+
+
 CREATE TABLE locations (  
-    location_id INT CHECK(length(location_id) = 1) PRIMARY KEY,  
+    location_id INTEGER CHECK(length(location_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
     name VARCHAR(255),  
-    address TEXT,  
+    address VARCHAR(20),  
     phone_number VARCHAR(20) CHECK(length(phone_number) = 8),  
-    email VARCHAR(255),  
+    email VARCHAR(255) CHECK(email LIKE '%@%'),  
     opening_hours VARCHAR(255) CHECK(length(opening_hours) = 10)  
 );  
   
 -- `members` table  
 CREATE TABLE members (  
-    member_id INT CHECK(length(member_id) = 1) PRIMARY KEY,  
+    member_id INTEGER CHECK(length(member_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
     first_name VARCHAR(255),  
     last_name VARCHAR(255),  
-    email VARCHAR(255),  
+    email VARCHAR(255) CHECK(email LIKE '%@%'),  
     phone_number VARCHAR(20) CHECK(length(phone_number) = 8),  
-    date_of_birth DATE CHECK(length(date_of_birth) = 10),  
-    join_date DATE CHECK(length(join_date) = 10),  
+    date_of_birth DATE TEXT CHECK(length(date_of_birth) = 10),  
+    join_date DATE TEXT CHECK(length(join_date) = 10),  
     emergency_contact_name VARCHAR(255),  
     emergency_contact_phone VARCHAR(20) CHECK(length(emergency_contact_phone) = 8)  
 );  
   
 -- `staff` table  
 CREATE TABLE staff (  
-    staff_id INT CHECK(length(staff_id) = 1) PRIMARY KEY,
+    staff_id INTEGER CHECK(length(staff_id) >= 1) PRIMARY KEY AUTOINCREMENT,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
-    email VARCHAR(255),
+    email VARCHAR(255) CHECK(email LIKE '%@%'),
     phone_number VARCHAR(20) CHECK(length(phone_number) = 8),  
-    position ENUM('Trainer', 'Manager', 'Receptionist', 'Maintenance'),
+    position VARCHAR(40),
     hire_date DATE CHECK(length(hire_date) = 10),
-    location_id INT CHECK(length(location_id) = 1),
+    location_id INTEGER CHECK(length(location_id) >= 1),
     FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );  
   
 -- `equipment` table  
 CREATE TABLE equipment (  
-    equipment_id INT CHECK(length(equipment_id) = 1) PRIMARY KEY,  
+    equipment_id INTEGER CHECK(length(equipment_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
     name VARCHAR(255),  
-    type ENUM('Cardio', 'Strength'),  
+    type VARCHAR(40),  
     purchase_date DATE CHECK(length(purchase_date) = 10),  
     last_maintenance_date DATE CHECK(length(last_maintenance_date) = 10),  
     next_maintenance_date DATE CHECK(length(next_maintenance_date) = 10),  
-    location_id INT CHECK(length(location_id) = 1),  
+    location_id INTEGER CHECK(length(location_id) >= 1),  
     FOREIGN KEY (location_id) REFERENCES locations(location_id)  
 );  
   
 -- `classes` table  
 CREATE TABLE classes (  
-    class_id INT CHECK(length(class_id) = 1) PRIMARY KEY,  
+    class_id INTEGER CHECK(length(class_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
     name VARCHAR(255),  
     description TEXT,  
-    capacity INT CHECK(length(capacity) = 2),  
-    duration INT CHECK(length(duration) = 2),  
-    location_id INT CHECK(length(location_id) = 1),  
+    capacity INTEGER CHECK(length(capacity) = 2),  
+    duration INTEGER CHECK(length(duration) = 2),  
+    location_id INTEGER CHECK(length(location_id) >= 1),  
     FOREIGN KEY (location_id) REFERENCES locations(location_id)  
 );  
   
 -- `class_schedule` table  
 CREATE TABLE class_schedule (  
-    schedule_id INT CHECK(length(schedule_id) = 1) PRIMARY KEY,  
-    class_id INT CHECK(length(class_id) = 1),  
-    staff_id INT CHECK(length(staff_id) = 1),  
+    schedule_id INTEGER CHECK(length(schedule_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    class_id INTEGER CHECK(length(class_id) >= 1),  
+    staff_id INTEGER CHECK(length(staff_id) >= 1),  
     start_time DATETIME CHECK(length(start_time) = 19),  
     end_time DATETIME CHECK(length(end_time) = 19),  
     FOREIGN KEY (class_id) REFERENCES classes(class_id),  
@@ -170,20 +185,20 @@ CREATE TABLE class_schedule (
   
 -- `memberships` table  
 CREATE TABLE memberships (  
-    membership_id INT CHECK(length(membership_id) = 1) PRIMARY KEY,  
-    member_id INT CHECK(length(member_id) = 1),  
+    membership_id INTEGER CHECK(length(membership_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
     type VARCHAR(255),  
     start_date DATE CHECK(length(start_date) = 10),
     end_date DATE CHECK(length(end_date) = 10),
-    status ENUM('Active', 'Inactive'),  
+    status VARCHAR(40),  
     FOREIGN KEY (member_id) REFERENCES members(member_id)
 );  
   
 -- `attendance` table  
 CREATE TABLE attendance (  
-    attendance_id INT CHECK(length(attendance_id) = 1) PRIMARY KEY,  
-    member_id INT CHECK(length(member_id) = 1),  
-    location_id INT CHECK(length(location_id) = 1),  
+    attendance_id INTEGER CHECK(length(attendance_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
+    location_id INTEGER CHECK(length(location_id) >= 1),  
     check_in_time DATETIME CHECK(length(check_in_time) = 19),  
     check_out_time DATETIME CHECK(length(check_out_time) = 19),
     FOREIGN KEY (member_id) REFERENCES members(member_id),  
@@ -192,30 +207,30 @@ CREATE TABLE attendance (
   
 -- `class_attendance` table  
 CREATE TABLE class_attendance (  
-    class_attendance_id INT CHECK(length(class_attendance_id) = 1) PRIMARY KEY,  
-    schedule_id INT CHECK(length(schedule_id) = 1),  
-    member_id INT CHECK(length(member_id) = 1),  
-    attendance_status ENUM('Registered', 'Attended', 'Unattended'),  
+    class_attendance_id INTEGER CHECK(length(class_attendance_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    schedule_id INTEGER CHECK(length(schedule_id) >= 1),  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
+    attendance_status VARCHAR(40),  
     FOREIGN KEY (schedule_id) REFERENCES class_schedule(schedule_id), 
     FOREIGN KEY (member_id) REFERENCES members(member_id)  
 );  
   
 -- `payments` table  
 CREATE TABLE payments (  
-    payment_id INT CHECK(length(payment_id) = 1) PRIMARY KEY,  
-    member_id INT CHECK(length(member_id) = 1),  
+    payment_id INTEGER CHECK(length(payment_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
     amount DECIMAL(10, 2),  
     payment_date DATE CHECK(length(payment_date) = 19),  
-    payment_method ENUM('Credit Card', 'Bank Transfer', 'PayPal'),  
-    payment_type ENUM('Monthly membership fee', 'Day pass'),  
+    payment_method VARCHAR(40),  
+    payment_type VARCHAR(40),  
     FOREIGN KEY (member_id) REFERENCES members(member_id)  
 );  
   
 -- `personal_training_sessions` table  
 CREATE TABLE personal_training_sessions (  
-    session_id INT CHECK(length(session_id) = 1) PRIMARY KEY,  
-    member_id INT CHECK(length(member_id) = 1),  
-    staff_id INT CHECK(length(staff_id) = 1),  
+    session_id INTEGER CHECK(length(session_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
+    staff_id INTEGER CHECK(length(staff_id) >= 1),  
     session_date DATE CHECK(length(session_date) = 10),  
     start_time TIME CHECK(length(start_time) = 8),  
     end_time TIME CHECK(length(end_time) = 8),  
@@ -226,8 +241,8 @@ CREATE TABLE personal_training_sessions (
   
 -- `member_health_metrics` table  
 CREATE TABLE member_health_metrics (  
-    metric_id INT CHECK(length(metric_id) = 1) PRIMARY KEY,  
-    member_id INT CHECK(length(member_id) = 1),  
+    metric_id INTEGER CHECK(length(metric_id) >= 1) PRIMARY KEY AUTOINCREMENT,  
+    member_id INTEGER CHECK(length(member_id) >= 1),  
     measurement_date DATE CHECK(length(measurement_date) = 10),  
     weight DECIMAL(5, 2),  
     body_fat_percentage DECIMAL(5, 2),  
@@ -238,11 +253,11 @@ CREATE TABLE member_health_metrics (
   
 -- `equipment_maintenance_log` table  
 CREATE TABLE equipment_maintenance_log ( 
-    log_id INT CHECK(length(log_id) = 1) NOT NULL PRIMARY KEY,  
-    equipment_id INT CHECK(length(equipment_id) = 1),
-    maintenance_date DATE CHECK(length(maintenance) = 10),
+    log_id INTEGER CHECK(length(log_id) >= 1) NOT NULL PRIMARY KEY AUTOINCREMENT,  
+    equipment_id INTEGER CHECK(length(equipment_id) >= 1),
+    maintenance_date DATE CHECK(length(maintenance_date) = 10),
     description TEXT,
-    staff_id INT CHECK(length(staff_id) = 1),
+    staff_id INTEGER CHECK(length(staff_id) >= 1),
     FOREIGN KEY (equipment_id) REFERENCES equipment(equipment_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
 );  
