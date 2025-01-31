a30bbba38375caf77c737fd16ec991f6b59f98c0
TomBett365 <tom.bettridge@ada.ac.uk>
2025-01-31 13:01:13 +0000

    completed majority of equipment_management tasks

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -3,15 +3,20 @@
 .mode column
 
 -- Enable foreign key support
-PRAGMA foreign_key = ON
+PRAGMA foreign_key = ON;
 
 -- Equipment Management Queries
 
 -- 1. Find equipment due for maintenance
--- TODO: Write a query to find equipment due for maintenance
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE (julianday(next_maintenance_date) - julianday(date('now'))) <= 30;
 
 -- 2. Count equipment types in stock
--- TODO: Write a query to count equipment types in stock
+SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, COUNT(DISTINCT name)
+FROM equipment
+GROUP BY equipment_type;
 
 -- 3. Calculate average age of equipment by type (in days)
--- TODO: Write a query to calculate average age of equipment by type (in days)
+SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, (julianday(date('now')) - julianday(purchase_date)) AS age_equipment
+FROM equipment;
