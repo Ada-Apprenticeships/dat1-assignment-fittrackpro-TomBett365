c242ad3f7f511830b60f4e9e65f63d638c4d01db
TomBett365 <tom.bettridge@ada.ac.uk>
2025-02-09 20:02:47 +0000

    equipment_management tasks completed

--- src/equipment_management.sql
+++ src/equipment_management.sql
@@ -13,10 +13,12 @@ FROM equipment
 WHERE (julianday(next_maintenance_date) - julianday(date('now'))) <= 30;
 
 -- 2. Count equipment types in stock
-SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, COUNT(DISTINCT name)
+SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, COUNT(DISTINCT name) AS count
 FROM equipment
 GROUP BY equipment_type;
 
 -- 3. Calculate average age of equipment by type (in days)
-SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, (julianday(date('now')) - julianday(purchase_date)) AS age_equipment
-FROM equipment;
+SELECT sub.equipment_type, AVG(age_equipment) AS avg_age_days
+FROM (SELECT RTRIM(RTRIM(RTRIM(RTRIM(name, '1'), '2'), '3'), '4') AS equipment_type, (julianday(date('now')) - julianday(purchase_date)) AS age_equipment
+      FROM equipment) sub
+GROUP BY sub.equipment_type;
