e7a8f0525c15b1b25d201ff26084dda730d8b54a
TomBett365 <tom.bettridge@ada.ac.uk>
2025-02-14 19:24:49 +0000

    Completed membership tasks

--- src/membership_management.sql
+++ src/membership_management.sql
@@ -8,42 +8,27 @@ PRAGMA foreign_key = ON;
 -- Membership Management Queries
 
 -- 1. List all active memberships
-
 SELECT m.member_id, m.first_name, m.last_name, ms.type AS membership_type, m.join_date
 FROM memberships ms
 JOIN members m ON m.member_id = ms.member_id;
 
 -- 2. Calculate the average duration of gym visits for each membership type
-
-/*
-SELECT ns.member_id, ns.no_sessions_per_member, AVG(no_sessions_per_member * duration_classes) AS avg_duration
-FROM (SELECT member_id, schedule_id, COUNT(*) AS no_sessions_per_member
-                        FROM class_attendance
-                        WHERE attendance_status = 'Attended'
-                        GROUP BY member_id) ns,
-        (SELECT schedule_id, AVG(ROUND((julianday(end_time) - julianday(start_time)) * 24 * 60)) AS duration_classes
-                        FROM class_schedule
-                        GROUP BY schedule_id) dc
-JOIN no_sessions ns ON ns.schedule_id = dc.schedule_id;
-
-
-SELECT m.membership_type, (SELECT AVG(duration)
-                            FROM average_duration ad
-                            GROUP BY member_id)
-FROM memberships m, (SELECT member_id, AVG(ROUND((julianday(end_time) - julianday(start_time)) * 24 * 60)) AS duration_personal_training
-                        FROM personal_training_sessions
-                        GROUP BY member_id),
-                    (SELECT member_id, schedule_id, COUNT(*) AS no_sessions_per_member
-                        FROM class_attendance
-                        WHERE attendance_status = 'Attended'
-                        GROUP BY member_id) ns, 
-                    (SELECT schedule_id, AVG(ROUND((julianday(end_time) - julianday(start_time)) * 24 * 60)) AS duration_classes
-                        FROM class_schedule
-                        GROUP BY schedule_id) dc
-*/
+SELECT membership_type, AVG(((no_classes_per_member * duration_of_class) + duration_sessions)) AS avg_visit_duration_minutes
+FROM (SELECT m.type AS membership_type, nc.member_id, nc.no_classes_per_member, dc.duration_of_class, ds.duration_sessions
+      FROM (SELECT member_id, schedule_id, COUNT(*) AS no_classes_per_member
+            FROM class_attendance 
+            WHERE attendance_status = 'Attended'
+            GROUP BY member_id) nc,
+            (SELECT schedule_id, ROUND((julianday(end_time) - julianday(start_time)) * 24 * 60) AS duration_of_class
+            FROM class_schedule
+            GROUP BY schedule_id) dc,
+            (SELECT member_id, SUM(ROUND((julianday(end_time) - julianday(start_time)) * 24 * 60)) AS duration_sessions
+            FROM personal_training_sessions
+            GROUP BY member_id) ds
+            JOIN memberships m ON nc.member_id = m.member_id AND nc.schedule_id = dc.schedule_id AND nc.member_id = ds.member_id)
+GROUP BY membership_type;
 
 -- 3. Identify members with expiring memberships this year
-
 SELECT m.member_id, m.first_name, m.last_name, m.email, ms.end_date
 FROM (SELECT member_id, end_date
       FROM memberships
