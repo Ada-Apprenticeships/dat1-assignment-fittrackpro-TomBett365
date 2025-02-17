3cc0247c83414e678c77857fde39e24905852c2f
TomBett365 <tom.bettridge@ada.ac.uk>
2025-02-17 11:06:33 +0000

    Checked/refactored staff management queries

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -8,15 +8,34 @@ PRAGMA foreign_key = ON;
 -- Staff Management Queries
 
 -- 1. List all staff members by role
-SELECT staff_id, first_name, last_name, position AS role
-FROM staff 
-ORDER BY role;
+SELECT 
+    staff_id, 
+    first_name, 
+    last_name, 
+    position AS role
+FROM 
+    staff
+ORDER BY 
+    role;
 
 -- 2. Find trainers with one or more personal training session in the next 30 days
-SELECT s.staff_id AS trainer_id, s.first_name || ' ' || s.last_name AS trainer_name, sc.session_count
-FROM staff s, (SELECT staff_id, COUNT(*) AS session_count
-               FROM personal_training_sessions
-               WHERE (julianday(session_date) - julianday(date('now'))) BETWEEN 0 AND 30
-               GROUP BY staff_id) sc
-JOIN staff ON sc.staff_id = s.staff_id
-GROUP BY sc.staff_id;
+SELECT 
+    s.staff_id AS trainer_id, 
+    s.first_name || ' ' || s.last_name AS trainer_name, 
+    sc.session_count
+FROM 
+    staff AS s
+JOIN 
+    (
+        SELECT 
+            staff_id, 
+            COUNT(*) AS session_count
+        FROM 
+            personal_training_sessions
+        WHERE 
+            (julianday(session_date) - julianday(DATE('now'))) BETWEEN 0 AND 30
+        GROUP BY 
+            staff_id
+    ) AS sc ON sc.staff_id = s.staff_id
+GROUP BY 
+    s.staff_id, s.first_name, s.last_name;
