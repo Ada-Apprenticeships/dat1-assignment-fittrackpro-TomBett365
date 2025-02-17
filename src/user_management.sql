8270c9106ea60672b3c3fdf755cf18d4f21940ca
TomBett365 <tom.bettridge@ada.ac.uk>
2025-02-17 09:08:42 +0000

    Checked/refactored user management tasks

--- src/user_management.sql
+++ src/user_management.sql
@@ -8,8 +8,14 @@ PRAGMA foreign_key = ON;
 -- User Management Queries
 
 -- 1. Retrieve all members
-SELECT member_id, first_name, last_name, email, join_date
-FROM members;
+SELECT
+    member_id, 
+    first_name, 
+    last_name, 
+    email, 
+    join_date
+FROM 
+    members;
 
 -- 2. Update a member's contact information
 UPDATE members
@@ -21,35 +27,98 @@ SELECT COUNT(member_id) AS member_count
 FROM members;
 
 -- 4. Find member with the most class registrations
-SELECT m.member_id, m.first_name, m.last_name, rc.registration_count  
-FROM (SELECT member_id, COUNT(*) AS registration_count  
-      FROM class_attendance  
-      GROUP BY member_id) rc  
+SELECT 
+    m.member_id, 
+    m.first_name, 
+    m.last_name, 
+    rc.registration_count
+FROM 
+    (
+        SELECT 
+            member_id, 
+            COUNT(*) AS registration_count
+        FROM 
+            class_attendance
+        GROUP BY 
+            member_id
+    ) AS rc
+JOIN 
+    members m 
+ON 
+    m.member_id = rc.member_id
+WHERE 
+    rc.registration_count = (
+        SELECT 
+            MAX(registration_count)
+        FROM 
+            (
+                SELECT 
+                    COUNT(*) AS registration_count
+                FROM 
+                    class_attendance
+                GROUP BY 
+                    member_id
+            ) AS registration_counts
+    );
 
-JOIN members m ON m.member_id = rc.member_id  
-WHERE rc.registration_count = (SELECT MAX(registration_count)  
-                               FROM (SELECT COUNT(*) AS registration_count  
-                                     FROM class_attendance
-                                     GROUP BY member_id) AS registration_counts
-);  
 -- 5. Find member with the least class registrations
-SELECT m.member_id, m.first_name, m.last_name, rc.registration_count  
-FROM (SELECT member_id, COUNT(*) AS registration_count
-      FROM class_attendance  
-      GROUP BY member_id) rc
-
-JOIN members m ON m.member_id = rc.member_id  
-WHERE rc.registration_count = (SELECT MIN(registration_count)  
-                               FROM (SELECT COUNT(*) AS registration_count
-                                     FROM class_attendance
-                                     GROUP BY member_id) AS registration_counts
-);
+SELECT 
+    m.member_id, 
+    m.first_name, 
+    m.last_name, 
+    rc.registration_count
+FROM 
+    (
+        SELECT 
+            member_id, 
+            COUNT(*) AS registration_count
+        FROM 
+            class_attendance
+        GROUP BY 
+            member_id
+    ) AS rc
+JOIN 
+    members m 
+ON 
+    m.member_id = rc.member_id
+WHERE 
+    rc.registration_count = (
+        SELECT 
+            MIN(registration_count)
+        FROM 
+            (
+                SELECT 
+                    COUNT(*) AS registration_count
+                FROM 
+                    class_attendance
+                GROUP BY 
+                    member_id
+            ) AS registration_counts
+    );
 
 -- 6. Calculate the percentage of members who have attended at least one class
-SELECT (CAST(attendance_member_count AS FLOAT) / member_count * 100) AS percentage_members
-FROM (SELECT COUNT(member_id) AS attendance_member_count, (SELECT COUNT(member_id) AS member_count
-                                                           FROM members) AS member_count
-      FROM (SELECT member_id, COUNT(*) AS attendance_count
-            FROM class_attendance
-            WHERE attendance_status = 'Attended'
-            GROUP BY member_id));
+SELECT 
+    (CAST(attendance_member_count AS FLOAT) / member_count * 100) AS percentage_members
+FROM 
+    (
+        SELECT 
+            COUNT(member_id) AS attendance_member_count, 
+            (
+                SELECT 
+                    COUNT(member_id) 
+                FROM 
+                    members
+            ) AS member_count
+        FROM 
+            (
+                SELECT 
+                    member_id, 
+                    COUNT(*) AS attendance_count
+                FROM 
+                    class_attendance
+                WHERE 
+                    attendance_status = 'Attended'
+                GROUP BY 
+                    member_id
+            ) AS attendance_count
+    );
