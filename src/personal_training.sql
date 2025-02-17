6379e89972a031333fc73a388b469dd5d989cc8d
TomBett365 <tom.bettridge@ada.ac.uk>
2025-02-17 11:18:20 +0000

    Checked/refactored personal training tasks

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -8,8 +8,23 @@ PRAGMA foreign_key = ON;
 -- Personal Training Queries
 
 -- 1. List all personal training sessions for a specific trainer
-SELECT pt.session_id, pt.member_id, pt.session_date, pt.start_time, pt.end_time
-FROM (SELECT * 
-      FROM staff
-      WHERE first_name = 'Ivy' AND last_name = 'Irwin') s
-JOIN personal_training_sessions pt ON s.staff_id = pt.staff_id;
+SELECT 
+    pt.session_id, 
+    m.first_name || ' ' || m.last_name AS member_name, 
+    pt.session_date, 
+    pt.start_time, 
+    pt.end_time
+FROM 
+    (
+        SELECT 
+            * 
+        FROM 
+            staff
+        WHERE 
+            first_name = 'Ivy' 
+            AND last_name = 'Irwin'
+    ) AS s
+JOIN 
+    personal_training_sessions AS pt ON s.staff_id = pt.staff_id
+JOIN 
+    members AS m ON pt.member_id = m.member_id;
